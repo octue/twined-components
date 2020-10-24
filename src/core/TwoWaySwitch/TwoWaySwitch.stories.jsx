4f4c8534db2fda2ee5ba9751df36d31502149922
Tom Clark <tom@octue.com>
2020-10-24 10:40:05 +0100

    IMP SplitButton component

--- src/core/TwoWaySwitch/TwoWaySwitch.stories.jsx
+++ src/core/TwoWaySwitch/TwoWaySwitch.stories.jsx
@@ -1,40 +1,29 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
-import { withMuiTheme } from '@harelpls/storybook-addon-materialui'
 
-import { addDecorator } from '@storybook/react'
-
-import { lightTheme, darkTheme } from '../../themes'
-
-// NOTE - SEE I'M IMPORTING THE UNWRAPPED COMPONENT
+// NOTE - IMPORTING THE UNWRAPPED COMPONENT
 // If you want to use is as if it were wrapped, you'll need to wrap it yourself here...
 import { TwoWaySwitch } from './TwoWaySwitch'
-
-// globally with custom themes
-addDecorator(withMuiTheme({
-  'Light theme': lightTheme,
-  'Dark theme': darkTheme,
-}))
-
 
 export default {
   title: 'Core/TwoWaySwitch',
   component: TwoWaySwitch,
-  decorators: [withMuiTheme()],
 }
 
 const Template = (args) => <TwoWaySwitch {...args} />
 
 
-export const Uncontrolled = Template.bind({})
-Uncontrolled.args = {
+export const Controlled = Template.bind({})
+Controlled.args = {
   leftLabel: 'Left Label',
   rightLabel: 'Right Label',
+  checked: true,
+  onCheckedChange: () => {},
 }
 
 
 export const NodeLabels = Template.bind({})
 NodeLabels.args = {
-  leftLabel: <Typography variant="h3">Typography Node As Left Label</Typography>,
+  leftLabel: <Typography variant="h6">Typography Node As Left Label</Typography>,
   rightLabel: <Typography variant="caption">Right Typography</Typography>,
 }
