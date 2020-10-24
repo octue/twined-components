4e6d897fd6e0de3fdd623b77d428e4c9ebea439c
Tom Clark <tom@octue.com>
2020-10-24 11:58:51 +0100

    IMP Added object ingredients and general structure for ingredients

---
+++ src/ingredients/elementMap.js
@@ -0,0 +1,8 @@
+// Map of all the javascript elements keyed by type
+import ObjectElement from './ObjectElement'
+
+const map = {
+  'object': ObjectElement,
+}
+
+export default map
---
+++ src/ingredients/object/ObjectCard/ObjectCard.stories.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Typography from '@material-ui/core/Typography'
+
+import ObjectCard from './ObjectCard'
+
+export default {
+  title: 'Ingredients/Object/Card',
+  component: ObjectCard,
+}
+
+const Template = (args) => <ObjectCard {...args} />
+
+
+export const Basic = Template.bind({})
+Basic.args = {
+}
